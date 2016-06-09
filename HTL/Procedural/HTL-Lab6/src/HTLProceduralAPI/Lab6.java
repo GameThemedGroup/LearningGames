3ed1d7674b6da47b8cb1d6b4f02f549fde75854f
Tom Lai <tomtclai@gmail.com>
2016-06-09 13:17:02 -0700

    HTL Procedural API

---
+++ HTL/Procedural/HTL-Lab6/src/HTLProceduralAPI/Lab6.java
@@ -0,0 +1,18 @@
+package HTLProceduralAPI;
+
+/**
+ * @author Tom Lai
+ * @author Jeen Cherdchusilp
+ */
+public class Lab6 extends HTLProceduralAPI
+{
+	
+	public Lab6()
+	{
+		super();
+	}
+	
+
+	
+
+}
