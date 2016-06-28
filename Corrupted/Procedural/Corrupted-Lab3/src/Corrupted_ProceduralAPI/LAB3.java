22469d82ae204a2765f680bc17a17615741afe8f
Taran <TaranChristensen@gmail.com>
2016-06-28 10:58:04 -0700

    Corrupted Pre-Migration Restructure

---
+++ Corrupted/Procedural/Corrupted-Lab3/src/Corrupted_ProceduralAPI/LAB3.java
@@ -0,0 +1,37 @@
+package Corrupted_ProceduralAPI;
+
+import Corrupted_ProceduralAPI.CorruptedProceduralAPI;
+
+/**
+ * LAB 3: If/Else Lab
+ * 
+ * @author Rachel Horton
+ */
+public class LAB3 extends CorruptedProceduralAPI
+{
+    
+   public void buildWorld()
+   {
+       
+   }
+   
+   public void updateWorld()
+   {
+       if(pressingRight())
+       {
+           drawTile(22,5, "green");
+       }
+       else if(pressingLeft())
+       {
+           drawTile(1,5, "red"); 
+       }
+       else if (pressingUp())
+       {
+           drawTile(8,8,"yellow");
+       }
+       else if (pressingDown())
+       {
+           drawTile(1,1,"blue");
+       }
+    }
+}
